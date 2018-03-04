23b14b34e2c4eefcef76aea1ae2528bfb0aa190b
Minh Hung <ngominhhung97@gmail.com>
2018-03-04 14:25:31 +0700

    add exercise 2

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number : ");
+            var number = Convert.ToInt32(Console.ReadLine());
+            switch (number) {
+                case 1: Console.WriteLine("Sunday");
+                    break;
+                case 2: Console.WriteLine("Monday");
+                    break;
+                case 3: Console.WriteLine("Tuesday");
+                    break;
+                case 4: Console.WriteLine("Wednesday");
+                    break;
+                case 5: Console.WriteLine("Thurday");
+                    break;
+                case 6: Console.WriteLine("Friday");
+                    break;
+                case 7: Console.WriteLine("Saturday");
+                    break;
+                default: Console.WriteLine("Enter correct number pls!!!");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
